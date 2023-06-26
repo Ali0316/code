5db6b8ab3811a766a119155362a1e6faa0a491dc
Muhammad Ali Nasir <mr.alinasir40@gmail.com>
2023-06-27 03:37:19 +0500

    Added Assessment 2

---
+++ assessment2.js
@@ -0,0 +1,41 @@
+/*Question 1: What is the current name of JavaScript?
+Answer: EcmaScript(ES6)
+
+Question 2: Write the symbols of logical operators?
+Answer: 
+1) &&(AND)
+2) ||(OR)
+3) ==(EQUAL)
+4) ===(EQUAL WITH SAME DATA TYPE)
+5) !(NOT)
+
+Question3: Print your name using JS?
+Answer: 
+
+function output(value){
+    console.log(`My name is ${value}`);
+}
+output("Ali");
+
+
+Question 4: Fix this code to return the multiplication result of 3 numbers also call the function
+function multiply  (a,b,c) {
+               a +b +c;
+}
+
+Answer: 
+function multiply(a,b,c){
+    return a*b*c;
+}
+
+
+Qusetion 5: Write a function to subtract the array elements
+Answer:
+
+function subtraction(arr){
+    let result=0;
+    arr.forEach((value)=>{
+        result-=value;
+    })
+}
+*/
